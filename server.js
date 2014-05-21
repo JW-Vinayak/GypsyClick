207d1149eb8b582a69f7acc2539169d9d073c925
JW-Vinayak <vinayak@junowallet.com>
2014-05-21 17:05:13 +0530

    added log

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 8080))
 //app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+	console.log('request received for base');
   response.send('Hello World!')
 })
 
+app.get('/index', function(request, response) {
+	console.log('request received for index');
+  response.send('Index request received!')
+})
+
+
 app.listen(app.get('port'),process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function() {
+	console.log('IP address is ' + process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
   console.log("Node app is running at localhost:" + app.get('port'))
 })
